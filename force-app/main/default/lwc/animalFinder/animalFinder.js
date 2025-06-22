f8bcd3251e44b2c9cdd5641f548db0b0c0a2f991
wsadkiewicz <248417@edu.p.lodz.pl>
2025-06-22 13:00:07 +0200

    PI-016 Lekkie poprawki wizualne wyszukiwarki zwierząt

--- force-app/main/default/lwc/animalFinder/animalFinder.js
+++ force-app/main/default/lwc/animalFinder/animalFinder.js
@@ -3,8 +3,11 @@ import getAnimals from '@salesforce/apex/AnimalFinderController.getAnimals';
 import getShelters from '@salesforce/apex/AnimalFinderController.getShelters';
 import getBreeds from '@salesforce/apex/AnimalFinderController.getBreeds';
 import noImage from '@salesforce/resourceUrl/no_image';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class AnimalSearch extends LightningElement {
+const ICON_PATH = "M13.5 0C20.9558 0 27 6.04416 27 13.5C27 20.9558 20.9558 27 13.5 27C6.04416 27 0 20.9558 0 13.5C0 6.04416 6.04416 0 13.5 0ZM11.9199 6.26562C11.448 6.2101 10.9698 6.28836 10.54 6.49121C10.1665 6.65584 9.84802 6.92505 9.62305 7.26562C9.39807 7.60631 9.27526 8.00485 9.27051 8.41309V18.5869C9.27526 18.9952 9.39807 19.3937 9.62305 19.7344C9.84802 20.0749 10.1665 20.3442 10.54 20.5088C10.885 20.6668 11.2603 20.7489 11.6396 20.75C12.2114 20.7506 12.7659 20.5544 13.21 20.1943L19.3721 15.1074C19.6137 14.915 19.8092 14.6698 19.9434 14.3916C20.0774 14.1134 20.1475 13.8088 20.1475 13.5C20.1475 13.1912 20.0774 12.8866 19.9434 12.6084C19.8092 12.3302 19.6137 12.085 19.3721 11.8926L13.21 6.80566C12.839 6.50847 12.392 6.32117 11.9199 6.26562Z";
+
+export default class AnimalSearch extends NavigationMixin(LightningElement) {
     selectedShelterId = '';
     selectedBreed = '';
     selectedAgeRange = '';
@@ -73,8 +76,17 @@ export default class AnimalSearch extends LightningElement {
 
     handleAnimalClick(event) {
         const animalId = event.currentTarget.dataset.id;
-        const url = `/lightning/r/Animal__c/${animalId}/view`;
-        window.open(url, '_blank');
+
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: animalId,
+                objectApiName: 'Animal__c',
+                actionName: 'view'
+            }
+        }).then((url) => {
+            window.open(url, "_blank");
+        });
     }
 
     handleFilterChange(event) {
@@ -82,5 +94,9 @@ export default class AnimalSearch extends LightningElement {
         const value = event.target.value;
 
         this[fieldName] = value;
+    }
+
+    get iconPath() {
+        return ICON_PATH;
     }
 }
